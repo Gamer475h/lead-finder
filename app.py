fbd0ae8f264211531cf9bbc89130b6b8a52b18be
Gamer475h <101051357+Gamer475h@users.noreply.github.com>
2026-06-25 19:27:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ with st.sidebar:
 if st.button("Lancer la recherche"):
     with st.spinner("Recherche en cours..."):
         overpass_url = "https://overpass-api.de/api/interpreter"
-        eaders = {'User-Agent': 'LeadFinderPro/1.0 (contact: elkhiderkaram190@gmail.com)'}
+        headers = {'User-Agent': 'LeadFinderPro/1.0 (contact: elkhiderkaram190@gmail.com)'}
         
         query = f"""
         [out:json];
